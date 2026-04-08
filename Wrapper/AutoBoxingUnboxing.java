e8687a8b5af369feb0eb9a01a9996a0523fcbdf2
DEEPESH <rajpootdeepesh259@gmail.com>
2026-04-08 15:19:27 +0530

    [DEEPESH] Wraper Added

---
+++ Wrapper/AutoBoxingUnboxing.java
@@ -0,0 +1,29 @@
+package WrapperClasses;
+import java.util.*;
+
+public class AutoBoxingUnboxing {
+
+    public static void autoboxing() {
+        List<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        System.out.println("List: " + list);
+
+        int sum = 0;
+
+        for (Integer x : list) {
+            sum = sum + x;
+        }
+
+        System.out.println("Sum Is: " + sum);
+    }
+
+    public static void main(String[] args) {
+        autoboxing();
+    }
+}
